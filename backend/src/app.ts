f5773bbf818c85c6343d19dd9aa02543f8f20028
firdoush <mdfirdoushali1776@gmail.com>
2024-08-13 11:56:56 +0530

    Added Authentication (JWT and token) and wrote user controllers and constants

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,14 @@ import { config } from 'dotenv'
 config();
 import morgan from "morgan";
 import appRouter from './routes';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 
 //middleware
 app.use(express.json()) //it tells the app that we will be using incoming and outgoing request
+app.use(cookieParser(process.env.COOKIE_SECRET))  //cookie parser is used to send cookies from backend to frontend
 
 //remove it in production
 app.use(morgan("dev"))
--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,6 +1,8 @@
 import User from "../models/Users.js"
 import { NextFunction, Request, Response } from "express";
 import {compare, hash} from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 //API to get all users from the db
 export const getAllUsers = async (req:Request, res:Response, next: NextFunction) => {
@@ -35,6 +37,30 @@ export const userSignup = async (req:Request, res:Response, next: NextFunction) 
         const user = new User({ name, email, password: hashedPassword })  //creating new instance of User
         await user.save();
 
+        //create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            domain: "localhost",
+            httpOnly: true,
+            signed: true, 
+            path: "/"
+    });
+
+    
+    // after correct password, token is generated
+    const token = createToken(user._id.toString(), user.email, "7d");
+
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true,
+        })
+
+
         return res
         .status(200)
         .json({message: "OK", id: user._id.toString()})
@@ -62,6 +88,29 @@ export const userLogin = async (req: Request, res: Response, next: NextFunction)
         if (!isPasswordCorrect) {
             return res.status(403).send("Incorrect password");
         }
+
+        //clears cookie after the user has loggedIn
+        res.clearCookie(COOKIE_NAME, {
+                domain: "localhost",
+                httpOnly: true,
+                signed: true, 
+                path: "/"
+        });
+
+        
+        // after correct password, token is generated
+        const token = createToken(user._id.toString(), user.email, "7d");
+
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+
+            res.cookie(COOKIE_NAME, token, {
+                path: "/",
+                domain: "localhost",
+                expires,
+                httpOnly: true,
+                signed: true,
+            })
 
         // Respond with success message
         return res.status(200).json({ message: "Login successful", userId: user._id.toString() });
